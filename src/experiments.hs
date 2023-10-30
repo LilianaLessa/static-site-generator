a8c0d539f9dbf3a38b8a1d375e93cae70951c082
Liliana Lessa <24816655+LilianaLessa@users.noreply.github.com>
2023-10-30 13:40:47 +0100

    moved html related functions to their own module

--- src/experiments.hs
+++ src/experiments.hs
@@ -6,10 +6,6 @@ getStructureString (Structure str) = str
 testStruct = Structure "hello world"
 testStr = getStructureString testStruct
 
-
-
-
-
 main :: IO ()
 main = putStrLn testStr
 
--- src/hello.hs
+++ src/hello.hs
@@ -1,45 +1,16 @@
-main = putStrLn (getStructureString myhtml)
+import Html
 
-getStructureString :: Structure -> String
-getStructureString (Structure str) = str
+main :: IO ()
+main = putStrLn (render myhtml)
 
-myhtml = append_ (Structure "test") (Structure "tset")
-
--- myhtml = 
---     makeHtml 
---         "Hello title" 
---         (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
-
-html_ :: String -> String
-html_ = el "html"
-
-body_ :: String -> String
-body_ = el "body"
-
-head_ :: String -> String
-head_ = el "head"
-
-title_ :: String -> String
-title_ = el "title"
-
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
-
-makeHtml title content = html_ (head_ (title_ title)) <> body_ content
-
--- tag generator function
-el :: String -> String -> String
-el tag content =
-      "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-
-newtype Structure = Structure String
-newtype Html = Html String
-
-
-
-append_ :: Structure -> Structure -> Structure
-append_ (Structure a) (Structure b) = Structure (a <> b)
+myhtml :: Html
+myhtml =
+  html_
+    "My title"
+    ( append_
+      (h1_ "Heading")
+      ( append_
+        (p_ "Paragraph #1")
+        (p_ "Paragraph #2")
+      )
+    )
